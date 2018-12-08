fa860647a0db6c2c762386473762675d15519e8b
nenjotsu <renenjo.valente@ubidy.com>
2018-12-08 23:46:38 +0800

    add noopener

--- src/common/containers/Home/components/ComingSoon.js
+++ src/common/containers/Home/components/ComingSoon.js
@@ -4,7 +4,11 @@ import '../../../css/ComingSoon.css';
 
 const ComingSoon = () => (
   <div className="cm-wrapper">
-    <a href="https://www.facebook.com/saijou.com.ph/" target="_blank">
+    <a
+      href="https://www.facebook.com/saijou.com.ph/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img
         className="cm-img"
         src={ComingSoonImg}
